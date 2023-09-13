27954472162ffdc2338dbbec8b42f710b6ab788e
GoldenLady <elenabuhaniuc@gmail.com>
2023-09-13 12:45:14 +0300

    Commit Please see the changes

---
+++ src/main/java/md/tekwillacademy/variablesprogramme/VariablesAndComments.java
@@ -0,0 +1,54 @@
+package md.tekwillacademy.variablesprogramme;
+
+import java.awt.*;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+        //Declare the variables
+        boolean isUsed;
+        boolean hasBattery;
+        boolean isAndroid;
+        boolean isStolen;
+        boolean isBlocked;
+        boolean isCharged;
+        //Char related
+        char condition;
+
+        //Numeric related
+        byte numberOfSimCards;
+        byte randomAccessMemory;
+        short productionYear;
+        int numberOfOwners;
+        long id;
+        float operationSystemVersion;
+        double price;
+
+        //Initialize an iPhone
+
+        isUsed = false;
+        hasBattery = true;
+        isAndroid = false;
+        isStolen = false;
+        isBlocked = true;
+        isCharged = true;
+
+        condition ='A';
+
+        numberOfSimCards = 2;
+        randomAccessMemory = 4;
+        productionYear = 2023;
+        numberOfOwners = 0;
+        id =  54661651654L;
+        operationSystemVersion = 16.16F;
+        price = 34550.5;
+
+        System.out.println("The phone is charged " + isCharged);
+        System.out.println("The phone price is " + price);
+
+
+
+
+
+    }
+}
